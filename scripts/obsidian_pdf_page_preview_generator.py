056bbbc1be6365e17f7b3fa98b59dba38eb0fd40
N0tGoingPro <87959691+N0tGoingPro@users.noreply.github.com>
2024-06-18 13:20:09 -0700

    Updating obsidian_pdf_page_preview_generator.py with heading level

--- scripts/obsidian_pdf_page_preview_generator.py
+++ scripts/obsidian_pdf_page_preview_generator.py
@@ -1,16 +1,18 @@
 # I use this to write notes for PDFs in Obsidian
 # PDF info
-file_name = "" # don't add .pdf at the end
-first_slide = -1
-last_slide = -2
+file_name = "Maarek - AWS Certified Cloud Practitioner" # don't add .pdf at the end
+first_page = -1
+last_page = -2
+heading_level = "" # E.g., ### or ####
 
 # print out file content
-current_slide = first_slide
-while current_slide <= last_slide:
-    print(f"![[{file_name}.pdf#page={current_slide}]]")
+current_page = first_page
+while current_page <= last_page:
+    print(f"{heading_level} Page {current_page}")
+    print(f"![[{file_name}.pdf#page={current_page}]]")
     print("- ")
     print("")
     print("")
     print("")
     print("")
-    current_slide = current_slide + 1
+    current_page = current_page + 1
